c84a6390fd2c16e7f21dec7636b8fec3f717fa79
Andrey Gunderin <ga@web-notes.info>
2021-05-08 17:09:02 +0300

    Step 4

--- src/utils/genreport.js
+++ src/utils/genreport.js
@@ -3,30 +3,30 @@ import getFilesData from './getfilesdata.js';
 
 const genReport = (filepath1, filepath2) => {
   const filesData = getFilesData(filepath1, filepath2);
+
   if (filesData) {
     const keys = Object.keys(filesData[0]).concat(Object.keys(filesData[1]));
     const sortedKeys = _.uniq(keys)
       .sort((a, b) => a.localeCompare(b));
     const result = [];
     sortedKeys.forEach((key) => {
-      const hasProp1 = Object.prototype.hasOwnProperty.call(filesData[0], key);
-      const hasProp2 = Object.prototype.hasOwnProperty.call(filesData[1], key);
-      if (hasProp1 && hasProp2) {
+      if (_.has(filesData[0], key) && _.has(filesData[1], key)) {
         if (filesData[0][key] === filesData[1][key]) {
           result.push(`  ${key}: ${filesData[0][key]}`);
         } else {
           result.push(`- ${key}: ${filesData[0][key]}`);
           result.push(`+ ${key}: ${filesData[1][key]}`);
         }
-      } else if (hasProp1 && !hasProp2) {
+      } else if (_.has(filesData[0], key) && !_.has(filesData[1], key)) {
         result.push(`- ${key}: ${filesData[0][key]}`);
-      } else if (!hasProp1 && hasProp2) {
+      } else if (!_.has(filesData[0], key) && _.has(filesData[1], key)) {
         result.push(`+ ${key}: ${filesData[1][key]}`);
       }
       return null;
     });
     console.log(`{\n  ${result.join('\n  ')}\n}`);
   }
+
   return null;
 };
 
