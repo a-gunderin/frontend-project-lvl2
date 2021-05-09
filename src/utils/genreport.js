a7988d38bdd4207486a9b1c616553a0a991f2f66
Andrey Gunderin <ga@web-notes.info>
2021-05-09 23:10:26 +0300

    Step 4

--- src/utils/genreport.js
+++ src/utils/genreport.js
@@ -1,32 +1,12 @@
-import _ from 'lodash';
 import getFilesData from './getfilesdata.js';
+import genDiff from './gendiff.js';
 
 const genReport = (filepath1, filepath2) => {
   const filesData = getFilesData(filepath1, filepath2);
 
   if (filesData) {
-    const keys = Object.keys(filesData[0]).concat(Object.keys(filesData[1]));
-    const sortedKeys = _.uniq(keys)
-      .sort((a, b) => a.localeCompare(b));
-    const result = [];
-    sortedKeys.forEach((key) => {
-      if (_.has(filesData[0], key) && _.has(filesData[1], key)) {
-        if (filesData[0][key] === filesData[1][key]) {
-          result.push(`  ${key}: ${filesData[0][key]}`);
-        } else {
-          result.push(`- ${key}: ${filesData[0][key]}`);
-          result.push(`+ ${key}: ${filesData[1][key]}`);
-        }
-      } else if (_.has(filesData[0], key) && !_.has(filesData[1], key)) {
-        result.push(`- ${key}: ${filesData[0][key]}`);
-      } else if (!_.has(filesData[0], key) && _.has(filesData[1], key)) {
-        result.push(`+ ${key}: ${filesData[1][key]}`);
-      }
-      return null;
-    });
-    console.log(`{\n  ${result.join('\n  ')}\n}`);
+    return genDiff(filesData[0], filesData[1]);
   }
-
   return null;
 };
 
