a2c10a4d98195d80aa8f926603764db269310670
Andrey Gunderin <ga@web-notes.info>
2021-05-11 01:37:42 +0300

    Step 4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,20 @@
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/utils/gendiff.js';
 import getFilesData from '../src/utils/getfilesdata.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-console.log(getFixturePath('file1.json'));
-console.log(getFixturePath('file2.json'));
+const filesData = getFilesData(getFixturePath('file1.json'), getFixturePath('file2.json'));
+const expectedFlat = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+test('Comparison of flat json files', () => {
+  expect(genDiff(filesData[0], filesData[1])).toBe(expectedFlat);
+});
