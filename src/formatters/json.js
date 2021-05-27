b44539469c22be8b311965f1ed9f864bbf8b5c6c
Andrey Gunderin <ga@web-notes.info>
2021-05-28 01:32:19 +0300

    refactor: refactoring of json.js

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,15 +1,13 @@
 import valueIsObj from '../utils/valueisobj.js';
 
 const formatSpacedKeys = (obj) => {
-  const keys = Object.keys(obj);
-  const result = keys.map((key) => {
-    const valueIsObject = valueIsObj(obj, key);
-    const keyIsSpaced = key.startsWith('  ');
-    if (keyIsSpaced) {
-      return { [key.substring(2)]: valueIsObject ? formatSpacedKeys(obj[key]) : obj[key] };
-    }
-    return { [key]: valueIsObject ? formatSpacedKeys(obj[key]) : obj[key] };
-  });
+  const result = Object.keys(obj)
+    .map((key) => {
+      const valueIsObject = valueIsObj(obj, key);
+      return key.startsWith('  ')
+        ? { [key.substring(2)]: valueIsObject ? formatSpacedKeys(obj[key]) : obj[key] }
+        : { [key]: valueIsObject ? formatSpacedKeys(obj[key]) : obj[key] };
+    });
   return Object.assign({}, ...result);
 };
 
